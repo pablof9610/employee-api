4288aab5ed179aeff8c97656aa062759f793df30
pafesan <pablof9610@gmail.com>
2024-08-19 16:19:11 -0300

    REFACTOR: handling custom exceptions and sending detailed object in response

--- src/main/java/com/personal/me/EmployeeManagement/exception/GlobalExceptionHandler.java
+++ src/main/java/com/personal/me/EmployeeManagement/exception/GlobalExceptionHandler.java
@@ -5,13 +5,52 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException noSuchElementException) {
-        return new ResponseEntity<>("Resource ID not found.", HttpStatus.NOT_FOUND);
+    public ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException exception) {
+        HashMap<String, Object> error = new HashMap<>();
+        error.put("message", "Resource id not found.");
+        error.put("details", exception.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(EmployeeNullPropException.class)
+    public ResponseEntity<Object> handleEmployeeNullPropException(EmployeeNullPropException exception) {
+        HashMap<String, Object> error = new HashMap<>();
+        String field = exception.getField();
+
+        switch (field) {
+            case "first name":
+                error.put("message", "First name field is empty.");
+                break;
+            case "last name":
+                error.put("message", "Last name field is empty.");
+                break;
+            case "specialization":
+                error.put("message", "Specialization field is empty.");
+                break;
+            default:
+                error.put("message", "A unknown error occurred.");
+                break;
+        }
+
+        error.put("details", exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(EmployeeWageValueException.class)
+    public ResponseEntity<Object> handleEmployeeWageValueException(EmployeeWageValueException exception) {
+        HashMap<String, Object> error = new HashMap<>();
+
+        error.put("message", "Wage must be greater than 500.");
+        error.put("details", exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
